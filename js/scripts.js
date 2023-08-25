d55283491dcd34427e996e5f42aa0e5ecb6ee5b7
JokerRueda <ruedasantiagoj@outlook.com>
2023-08-25 14:51:11 -0500

    Formulario
    Cree los campos del formulario y dejé preparado la parte de LocalStorage

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+/*Guardar*/
+$(document).ready(function(){    
+    $('#boton-guardar').click(function(){        
+        /*Captura de datos escrito en los inputs*/        
+        var nom = document.getElementById("nombretxt").value;
+        var apel = document.getElementById("apellidotxt").value;
+        /*Guardando los datos en el LocalStorage*/
+        localStorage.setItem("Nombre", nom);
+        localStorage.setItem("Apellido", apel);
+        /*Limpiando los campos o inputs*/
+        document.getElementById("nombretxt").value = "";
+        document.getElementById("apellidotxt").value = "";
+    });   
+});
+/*Cargar*/
+$(document).ready(function(){    
+    $('#boton-cargar').click(function(){                       
+        /*Obtener datos almacenados*/
+        var nombre = localStorage.getItem("Nombre");
+        var apellido = localStorage.getItem("Apellido");
+        /*Mostrar datos almacenados*/      
+        document.getElementById("nombre").innerHTML = nombre;
+        document.getElementById("apellido").innerHTML = apellido; 
+    });   
+});
